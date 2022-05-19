4dddaff5aad53010db71946f8efaaa76ab25a9a8
jvitoralb <jvvalb@gmail.com>
2022-05-19 15:36:09 -0300

    Upload project

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import { quotesBank } from "./quotesBank.js";
+
+const colors = [
+    '#87A9FF',
+    '#37B7B7',
+    '#8EEAEA',
+    '#775DBF',
+    '#F3C66F',
+    '#F39C12',
+    '#DD7267',
+    '#FB6964'
+];
+
+const quoteAuthor = (index) => {
+    let quote = quotesBank[index].quote;
+    let author = quotesBank[index].author;
+
+    $('#text').html(quote);
+    $('#author').html('&#8212 ' + author);
+    $('#tweet-quote').attr('href', `https://twitter.com/intent/tweet?text=${quote}%20—${author}`);
+}
+
+const randomNum = (type) => {
+    if (type === 'colors') {
+        return Math.floor(Math.random() * colors.length);
+    }
+    return Math.floor(Math.random() * quotesBank.length);
+}
+
+const styleBg = () => {
+    let color = randomNum('colors');
+
+    $('.bg').css('background-color', colors[color]);
+    $('#tweet-quote').hover(function () {
+        $(this).css('background-color', 'rgba(230, 243, 255, 0.5)');
+    }, function () {
+        $(this).css('background-color', colors[color]);
+    });
+}
+
+const newQuote = $('#new-quote');
+let lastIndex;
+
+const getQuote = () => {
+    let quoteAt = randomNum();
+    lastIndex = quoteAt;
+    quoteAuthor(quoteAt);
+}
+
+// Get a quote as soon as the page loads.
+$(() => getQuote());
+
+newQuote.click(function getNewQuote() {
+    let newQuoteAt = randomNum();
+
+    if (newQuoteAt === lastIndex){
+        getNewQuote();
+    } else {
+        quoteAuthor(newQuoteAt);
+        lastIndex = newQuoteAt;
+        styleBg();
+    }
+});
+
+newQuote.on('mousedown', function() {
+    let defaultShadow = '0 1px 3px rgba(0, 0, 0, 0.65)';
+
+    $(this).css('box-shadow', 'inset 0 0 3px rgba(0, 0, 0, 0.65)');
+    newQuote.on('mouseup', function() {
+        $(this).css('box-shadow', defaultShadow);
+    });
+    newQuote.on('mouseleave', function() {
+        $(this).css('box-shadow', defaultShadow);
+    });
+});
+
---
+++ src/quotesBank.js
@@ -0,0 +1,109 @@
+export const quotesBank = [
+    {
+        quote: "Sometimes even to live is an act of courage.",
+        author: "Sêneca"
+    }, {
+        quote: "True happiness is to enjoy the present, without anxious dependence upon the future, not to amuse ourselves with either hopes or fears but to rest satisfied with what we have, which is sufficient, for he that is so wants nothing. The greatest blessings of mankind are within us and within our reach. A wise man is content with his lot, whatever it may be, without wishing for what he has not.",
+        author: "Sêneca"
+    }, {
+        quote: "All cruelty springs from weakness.",
+        author: "Sêneca"
+    }, {
+        quote: "Religion is regarded by the common people as true, by the wise as false, and by rulers as useful.",
+        author: "Sêneca"
+    }, {
+        quote: "The mind cannot foresee its own advance.",
+        author: "Friedrich A. von Hayek"
+    }, {
+        quote: "Knowledge is naturally dispersed throughout society.",
+        author: "Friedrich A. von Hayek"
+    }, {
+        quote: "We shall not grow wiser before we learn that much that we have done was very foolish.",
+        author: "Friedrich A. von Hayek"
+    }, {
+        quote: "La morale est la faiblesse de la cervelle.",
+        author: "Arthur Rimbaud"
+    }, {
+        quote: "Plus fortes que l'alcool, plus vastes que nos lyres, Fermentent les rousseurs amères de l'amour.",
+        author: "Arthur Rimbaud"
+    }, {
+        quote: "Le monde a soif d'amour : tu viendras l'apaiser.",
+        author: "Arthur Rimbaud"
+    }, {
+        quote: "On n'est pas sérieux, quand on a dix-sept ans.",
+        author: "Arthur Rimbaud"
+    }, {
+        quote: "Elle ne savait pas que l'Enfer, c'est l'absence",
+        author: "Paul Verlaine"
+    }, {
+        quote: "Les sanglots longs des violons de l'automne blessent mon coeur d'une langueur monotone. Tout suffocant et blême, quand sonne l'heure, je me souviens des jours anciens et je pleure ; et je m'en vais au vent mauvais qui m'emporte deçà, delà, pareil à la feuille morte.",
+        author: "Paul Verlaine"
+    }, {
+        quote: "Il ne faut jamais juger les gens sur leurs fréquentations. Tenez, Judas, par exemple, il avait des amis irréprochables.",
+        author: "Paul Verlaine"
+    }, {
+        quote: "A certeza é própria do caráter raso.",
+        author: "Leandro Karnal"
+    }, {
+        quote: "A zona de conforto é a grande armadilha da vida.",
+        author: "Leandro Karnal"
+    }, {
+        quote: "Faça, porque se você não fizer, em breve, o resto será silêncio.",
+        author: "Leandro Karnal"
+    }, {
+        quote: "Cuidado com gente que não tem dúvida. Gente que não tem dúvida não é capaz de inovar, de reinventar, não é capaz de fazer de outro modo. Gente que não tem dúvida só é capaz de repetir.",
+        author: "Mário Sergio Cortella"
+    }, {
+        quote: "É um fraco que teme aquele que não é igual e se sente ameaçado por ele.",
+        author: "Mário Sergio Cortella"
+    }, {
+        quote: "Na vida é preciso ter raiz, não âncora. A raiz te alimenta, a âncora te imobiliza.",
+        author: "Mário Sergio Cortella"
+    }, {
+        quote: "O tempo às vezes é alheio à nossa vontade, mas só o que é bom dura tempo o bastante pra se tornar inesquecível.",
+        author: "Charlie Brown Jr."
+    }, {
+        quote: "A vida me ensinou a nunca desistir, nem ganhar, nem perder, mas procurar evoluir.",
+        author: "Charlie Brown Jr."
+    }, {
+        quote: "Strive not to be a success, but rather to be of value.",
+        author: "Albert Einstein"
+    }, {
+        quote: "The most difficult thing is the decision to act, the rest is merely tenacity.",
+        author: "Amelia Earhart"
+    }, {
+        quote: "The most common way people give up their power is by thinking they don’t have any.",
+        author: "Alice Walker"
+    }, {
+        quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
+        author: "Chinese Proverb"
+    }, {
+        quote: "Do not let your fire go out, spark by irreplaceable spark in the hopeless swamps of the not-quite, the not-yet, and the not-at-all. Do not let the hero in your soul perish in lonely frustration for the life you deserved and have never been able to reach. The world you desire can be won. It exists.. it is real.. it is possible.. it's yours.",
+        author: "Ayn Rand"
+    }, {
+        quote: "If you don't know, the thing to do is not to get scared, but to learn.",
+        author: "Ayn Rand"
+    }, {
+        quote: "Learn to value yourself, which means: fight for your happiness.",
+        author: "Ayn Rand"
+    }, {
+        quote: "Freedom (n.): To ask nothing. To expect nothing. To depend on nothing.",
+        author: "Ayn Rand"
+    }, {
+        quote: "The truth is not for all men but only for those who seek it.",
+        author: "Ayn Rand"
+    }, {
+        quote: "A creative man is motivated by the desire to achieve, not by the desire to beat others.",
+        author: "Ayn Rand"
+    }, {
+        quote: "A felicidade só é real quando compartilhada.",
+        author: "Christopher McCandless"
+    }, {
+        quote: "Every child is an artist.  The problem is how to remain an artist once he grows up.",
+        author: "Pablo Picasso"
+    }, {
+        quote: "Princes and governments are far more dangerous than other elements within society.",
+        author: "Niccolò Machiavelli"
+    }
+];
+
